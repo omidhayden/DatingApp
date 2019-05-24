db3cf555360990ff852042493ab9aca88de9955e
Omid <omidhayden@gmail.com>
2019-05-24 00:26:02 -0400

    initial comit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,13 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public Value()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
